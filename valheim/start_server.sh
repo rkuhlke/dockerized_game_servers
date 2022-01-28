ce143b159f136d07347722c48faeda0e5dd524fd
Robert Kuhlke <bkuhlke@yahoo.com>
2022-01-27 21:42:56 -0500

    fixing vulnerabilities in valheim server

--- valheim/start_server.sh
+++ valheim/start_server.sh
@@ -9,6 +9,8 @@ export serverPassword=$serverPassword
 
 echo "Starting server PRESS CTRL-C to exit"
 
+# curl ipv4.icanhazip.com
+
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
